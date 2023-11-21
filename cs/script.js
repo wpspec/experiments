9166ddf5fc4e86b3c1b7be735a65cbfbdbf6af9b
unknown <109889233+wpspec@users.noreply.github.com>
2023-11-21 21:08:06 +0800

    Implement janky physics

--- cs/script.js
+++ cs/script.js
@@ -7,7 +7,9 @@ const player = {
   width: 20,
   height: 20,
   color: '#00F',
-  speed: 0.05,
+  speed: 0,
+  acceleration: 0.01,
+  mass: 1,
 };
 
 const minions = [];
@@ -15,12 +17,13 @@ const minionCount = 7;
 
 for (let i = 0; i < minionCount; i++) {
   minions.push({
-    x: canvas.width - 50,
+    x: canvas.width - 50 - i * 30,
     y: canvas.height - 30,
     width: 20,
     height: 20,
     color: '#F00',
     speed: 2,
+    mass: 1,
   });
 }
 
@@ -36,28 +39,38 @@ function drawPlayer() {
 function drawMinions() {
   for (const minion of minions) {
     ctx.fillStyle = minion.color;
-    ctx.fillRect(minion.x, minion.y, minion.width, minion.height);
+    ctx.fillRect(minion.x - minion.width / 2, minion.y - minion.height / 2, minion.width, minion.height);
   }
   
 }
 
 function update() {
 
-  player.x = lerp(player.x, targetX, player.speed);
-  player.y = lerp(player.y, targetY, player.speed);
+  player.speed += (targetX - player.x) * player.acceleration;
+  player.x += player.speed;
 
   for (const minion of minions) {
     minion.x -= minion.speed;
   }
 
   for (const minion of minions) {
-    if (
-      player.x - player.width / 2 < minion.x + minion.width &&
-      player.x + player.width / 2 > minion.x &&
-      player.y - player.height / 2 < minion.y + minion.height &&
-      player.y + player.height / 2 > minion.y
-    ) {
-      resetMinions();
+    const dx = player.x - minion.x;
+    const dy = player.y - minion.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    const combinedRadius = (player.width + minion.width) / 2;
+
+    if (distance < combinedRadius) {
+      const angle = Math.atan2(dy, dx);
+      const overlap = combinedRadius - distance;
+
+      player.x += overlap * Math.cos(angle);
+      minion.x -= overlap * Math.cos(angle);
+
+      const relativeVelocity = player.speed - minion.speed;
+      const impulse = 2 * relativeVelocity / (player.mass + minion.mass);
+
+      player.speed -= impulse * minion.mass;
+      minion.speed += impulse * player.mass;
     }
   }
 
@@ -71,14 +84,9 @@ function update() {
 }
 
 function resetMinions() {
-  const initialX = canvas.width - 50;
   for (let i = 0; i < minionCount; i++) {
-    minions[i].x = initialX - i * 30;
+    minions[i].x = canvas.width - 50 - i * 30;
   }
-}
-
-function lerp(start, end, t) {
-  return start * (1 - t) + end * t;
 }
 
 function gameLoop() {
